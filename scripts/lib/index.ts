998cefeb409186982235e9716ed5dcaaf495105e
dladislav201 <dobrodii.vlad201@gmail.com>
2025-06-20 19:01:39 +0200

    chore(scripts): handle err in logger

--- scripts/lib/index.ts
+++ scripts/lib/index.ts
@@ -1,3 +1,2 @@
 export { logger } from './logger';
 export { getPackagePath } from './getPackagePath';
-export { logExecError } from './logExecError';
--- scripts/lib/logger.ts
+++ scripts/lib/logger.ts
@@ -1,15 +1,33 @@
+import util from 'util';
 import chalk from 'chalk';
+import { isExecError } from './isExecError';
 
 const prefix = chalk.gray('[build] »');
 
 export const logger = {
-  log: (message: string) => console.log(`${prefix} ${message}`),
+  log: (message: string) =>
+    console.log(`${prefix} ${chalk.blue('ℹ')} ${message}`),
   success: (message: string) =>
     console.log(`${prefix} ${chalk.green('✔')} ${message}`),
-  error: (error: unknown) =>
-    console.error(
-      `${prefix} ${chalk.red('✖')} ${error instanceof Error ? error.message : error}`,
-    ),
+  error: (message: string, err?: unknown) => {
+    console.error(`${prefix} ${chalk.red('✖')} ${message}`);
+
+    if (err == null) return;
+
+    if (isExecError(err)) {
+      if (err.stdout) console.error(`${prefix} ${chalk.gray(err.stdout)}`);
+      if (err.stderr) console.error(`${prefix} ${chalk.red(err.stderr)}`);
+      console.error(
+        `${prefix} ${chalk.yellow(`Exit code: ${err.code ?? 'unknown'}`)}`,
+      );
+    } else if (err instanceof Error) {
+      console.error(`${prefix} ${err.stack}`);
+    } else {
+      console.error(
+        `${prefix} ${util.inspect(err, { depth: null, colors: true })}`,
+      );
+    }
+  },
   warn: (message: string) =>
     console.log(`${prefix} ${chalk.yellow('⚠')} ${message}`),
 };
