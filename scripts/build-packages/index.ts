273a836666962b542b4e02b3a1f5f37549fdd683
dladislav201 <dobrodii.vlad201@gmail.com>
2025-06-20 19:11:00 +0200

    refactor(scripts): move buildAllPackage logic to index and handle err from buildPackage

--- scripts/build-packages/index.ts
+++ scripts/build-packages/index.ts
@@ -1,8 +1,26 @@
+import chalk from 'chalk';
+import { buildPackage } from './buildPackage';
 import { logger } from '../lib';
-import { buildAllPackages } from './buildAllPackages';
 
-buildAllPackages().catch((error) => {
-  logger.error('Build process failed');
-  console.error(error);
-  process.exit(1);
-});
+const BUILD_ORDER = ['core', 'next'];
+
+async function main() {
+  try {
+    const startTime = Date.now();
+    logger.log('Build initiated');
+
+    for (const pkg of BUILD_ORDER) {
+      await buildPackage(pkg);
+    }
+
+    const totalTime = ((Date.now() - startTime) / 1000).toFixed(2);
+    logger.success(
+      `All packages built successfully in ${chalk.green(`${totalTime}s`)}`,
+    );
+  } catch (err: unknown) {
+    logger.error('Build failed with error: ', err);
+    process.exit(1);
+  }
+}
+
+main();
